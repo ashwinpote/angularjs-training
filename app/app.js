e0f53ac7a6af24685ce829503fa5642949605570
ashwin pote <ashwin.pote@globant.com>
2016-02-24 00:08:30 +0530

    updated the controller, service unit testing code in main stream

--- app/app.js
+++ app/app.js
@@ -1,6 +1,37 @@
-angular.module('myapp', [])
-.filter('reverse',[function(){
-    return function(string){
-        return string.split('').reverse().join('');
+angular.module('myapp', []).controller('MyCtrl', ['$scope', function($scope) {
+    $scope.orderByField = 'transactionId';
+    $scope.reverseSort = false;
+    $scope.notificationMsg = "No Notification...";
+
+    //***********For income/expense data load from json***************************// 
+    mainService.getincomejson().then(function(data) {
+        mainService.incomesData = data;
+    });
+
+    mainService.getexpensejson().then(function(data) {
+        mainService.expenseData = data;
+    });
+    //***********End function*******************************************************//
+
+    //***********For income/expense calculate totals *******************************// 
+    function initTotal() {
+        mainService.incomeTotal = mainService.getTotal("income");
+        mainService.expenseTotal = mainService.getTotal("expense");
+        $scope.mainincomeTotal = parseFloat(mainService.incomeTotal).toFixed(2);
+        $scope.mainexpenseTotal = parseFloat(mainService.expenseTotal).toFixed(2);
+        $scope.mainTotal = parseFloat($scope.mainincomeTotal - $scope.mainexpenseTotal).toFixed(2);
     }
-}])
+    //***********End function*******************************************************//
+
+    //***********For calculate totals from child scope *****************************// 
+    $rootScope.$on('handleTotal', function(event, args) {
+        initTotal();
+    });
+    //***********End function*******************************************************//
+
+    //***********For notification from child scope *********************************// 
+    $rootScope.$on('handlenotify', function(event, args) {
+        $scope.notificationMsg = args;
+    });
+    //***********End function*******************************************************//
+}]);
--- test/test.js
+++ test/test.js
@@ -1,17 +1,22 @@
-describe('Filters', function() { 
-    beforeEach(module('myapp')); 
-
-    describe('reverse', function() { 
-
-        var reverse;
-        beforeEach(inject(function($filter) { 
-            reverse = $filter('reverse', {});
-        }));
-
-        it('Should reverse a string', function() { 
-            expect(reverse('ashwin')).toBe('niwhsa'); 
-        });
-
+describe('myapp', function() {
+    var scope,
+        controller;
+    beforeEach(function() {
+        angular.mock.module('myapp');
     });
 
+    describe('MyCtrl', function() {
+        beforeEach(inject(function($rootScope, $controller,  _mainService_) {
+            scope = $rootScope.$new();
+            mainService = _mainService_;
+            controller = $controller('MyCtrl', {
+                '$scope': scope
+            });
+        }));
+        it('notification text check', function() {
+            expect(scope.notificationMsg).toBe('No Notification...');
+        });
+
+        spyOn(mainService, 'getTotal').andReturn(Total);
+    });
 });
